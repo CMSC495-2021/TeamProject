5d5715013dfb8a1882d0619c0b5d8e52ba1f5571
donaldirvine <irvine.dj@icloud.com>
2021-01-31 06:46:20 -0700

    - Added Local DynamoDB Files - Created/Started Guide on how to use it (mac) - Added AWS CLI package (mac) - Put non-related files on .ebignore (for beanstalk deployment)
    - Played with CSS a bit, and added a couple Javascript Artifacts.

--- application.py
+++ application.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 
-application = app = Flask(__name__)
-
+application = app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
