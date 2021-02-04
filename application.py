1fd670c05b634cdf9158b58aa9a08c6ffa7f9d27
donaldirvine <irvine.dj@icloud.com>
2021-02-04 08:55:52 -0700

    Added the register page and linked the login page to it... Just used the same idea Rebecca had.
    Added backend flask code to serve new page.

--- application.py
+++ application.py
@@ -11,6 +11,10 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 
 
 if __name__ == '__main__':
