392bb36e8494ea8c1a9a3d3b934bf60569f543cf
donaldirvine <irvine.dj@icloud.com>
2021-02-03 18:08:00 -0700

    Added python flask code to serve at /login

--- application.py
+++ application.py
@@ -6,6 +6,11 @@ application = app = Flask(__name__, static_folder='static', static_url_path='/st
 def index():
     return render_template("index.html")
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run()
 
