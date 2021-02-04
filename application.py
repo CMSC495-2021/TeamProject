31ebbb434f6a71a03c066b1ae6c80ecfd057aa9a
donaldirvine <irvine.dj@icloud.com>
2021-02-03 18:25:30 -0700

    made login the default route as well '/'

--- application.py
+++ application.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template
 
 application = app = Flask(__name__, static_folder='static', static_url_path='/static')
 
-@app.route('/', methods=["GET"])
+@app.route('/index', methods=["GET"])
 def index():
     return render_template("index.html")
 
 @app.route('/login', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
 
 
 if __name__ == '__main__':
