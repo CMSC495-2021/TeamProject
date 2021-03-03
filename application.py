f436246f4f1702bd3d21ee992a87ff596f0f9964
JD Moore <jd.moore@jdmtechsolutions.com>
2021-03-03 00:02:00 -0600

    Prod DB restored

--- application.py
+++ application.py
@@ -37,38 +37,38 @@ socketIO = SocketIO(app)
 LOG = create_logger(application)
 
 # crypto items
-# class CryptoItems:
-#     aws_access_key_id = "ASIARSOHCYSCN7AHG5X2"
-#     aws_secret_access_key = "qmFfarnCi7K4ON37Jux+EuUJn5hjlyrOdvzrOmY2"
-#     aws_session_token = "FwoGZXIvYXdzEPv//////////wEaDNkZQxRhOvS2k8GCLCLIASjGAKe7" \
-#                         "AQYzNg0VPfz5aMJdm9BVT/fEY9NoSQkjkZaCK4gciMrO6WeJ6p2Ehkexu1GKwMnlfCI9h2BB" \
-#                         "wZmt65d/YZAffSt+SVrVbhm5Qhj82K0+JokaH5yD7irsJ1cHVLMe45YC5s6V5p0melYlFm0qy" \
-#                         "wmoYUjRg+FTn3uK6nfT9KrlYubWL5PMx8AHI5BHr9deIuVZ6uBXTr75631WSVd9hjgFpdE+SB" \
-#                         "Es58RBKai3WN6px2Qs2AdXPJ0E1PzjF0dgPVeB7sZEKOCB1YEGMi310efByoaL/P18v/s8BJQW" \
-#                         "hgqHHDBQI1RXdb4k6/m6ivWe94D4cD+eNewO/ac="
+class CryptoItems:
+    aws_access_key_id = "ASIARSOHCYSCN7AHG5X2"
+    aws_secret_access_key = "qmFfarnCi7K4ON37Jux+EuUJn5hjlyrOdvzrOmY2"
+    aws_session_token = "FwoGZXIvYXdzEPv//////////wEaDNkZQxRhOvS2k8GCLCLIASjGAKe7" \
+                        "AQYzNg0VPfz5aMJdm9BVT/fEY9NoSQkjkZaCK4gciMrO6WeJ6p2Ehkexu1GKwMnlfCI9h2BB" \
+                        "wZmt65d/YZAffSt+SVrVbhm5Qhj82K0+JokaH5yD7irsJ1cHVLMe45YC5s6V5p0melYlFm0qy" \
+                        "wmoYUjRg+FTn3uK6nfT9KrlYubWL5PMx8AHI5BHr9deIuVZ6uBXTr75631WSVd9hjgFpdE+SB" \
+                        "Es58RBKai3WN6px2Qs2AdXPJ0E1PzjF0dgPVeB7sZEKOCB1YEGMi310efByoaL/P18v/s8BJQW" \
+                        "hgqHHDBQI1RXdb4k6/m6ivWe94D4cD+eNewO/ac="
 
-#     dbResource = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id,
-#                                 aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token,
-#                                 region_name='us-east-1').Table('Users')
+    dbResource = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id,
+                                aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token,
+                                region_name='us-east-1').Table('Users')
 
-#     # crypto key and material provider
-#     aws_cmk_id = 'arn:aws:kms:us-east-1:108328567940:key/ac31eb77-1a66-452f-9744-8aec26b9aa74'
-#     aws_kms_cmp = AwsKmsCryptographicMaterialsProvider(key_id=aws_cmk_id)
+    # crypto key and material provider
+    aws_cmk_id = 'arn:aws:kms:us-east-1:108328567940:key/ac31eb77-1a66-452f-9744-8aec26b9aa74'
+    aws_kms_cmp = AwsKmsCryptographicMaterialsProvider(key_id=aws_cmk_id)
 
-#     # how the crypto is applied to attributes
-#     crypto_actions = AttributeActions(
-#         default_action=CryptoAction.DO_NOTHING,
-#         attribute_actions={
-#             'password': CryptoAction.ENCRYPT_AND_SIGN})
+    # how the crypto is applied to attributes
+    crypto_actions = AttributeActions(
+        default_action=CryptoAction.DO_NOTHING,
+        attribute_actions={
+            'password': CryptoAction.ENCRYPT_AND_SIGN})
 
-#     crypto_context = EncryptionContext(table_name='Users')
+    crypto_context = EncryptionContext(table_name='Users')
 
-#     custom_crypto_config = CryptoConfig(materials_provider=aws_kms_cmp,
-#                                         attribute_actions=crypto_actions,
-#                                         encryption_context=crypto_context)
+    custom_crypto_config = CryptoConfig(materials_provider=aws_kms_cmp,
+                                        attribute_actions=crypto_actions,
+                                        encryption_context=crypto_context)
 
-#     encrypted_resource = EncryptedTable(table=dbResource, materials_provider=aws_kms_cmp,
-#                                         attribute_actions=crypto_actions)
+    encrypted_resource = EncryptedTable(table=dbResource, materials_provider=aws_kms_cmp,
+                                        attribute_actions=crypto_actions)
 
 #FIXME TEST DB for session testing... DO NOT IMPLEMENT IN PROD!!!
 users = {
@@ -164,27 +164,27 @@ def Authenticate():
                 session["INITIALS"] = user["initials"]
                 return redirect(url_for('chatmain'))
 
-            # response = CryptoItems.encrypted_resource.query(
-            #     KeyConditionExpression=Key('UserName').eq(userName),
-            #     crypto_config=CryptoItems.custom_crypto_config
-            # )
-            # try:
-            #     items = response['Items']
-            #     pw = items[0]['password']
-            #     try:
-            #         if password == pw:
-            #             flash('Success!', 'Success')
-            #             return redirect(url_for('chatmain'))
-            #             #Load user obj into session for use...
-            #         else:
-            #             flash('Bad UserName/Password', 'Failed')
-            #             return redirect(url_for('login'))
-            #     except:
-            #         flash('Bad UserName/Password', 'Failed')
-            #         return redirect(url_for('login'))
-            # except:
-            #     flash('Bad UserName/Password', 'Failed')
-            #     return redirect(url_for('login'))
+            response = CryptoItems.encrypted_resource.query(
+                KeyConditionExpression=Key('UserName').eq(username),
+                crypto_config=CryptoItems.custom_crypto_config
+            )
+            try:
+                items = response['Items']
+                pw = items[0]['password']
+                try:
+                    if password == pw:
+                        flash('Success!', 'Success')
+                        return redirect(url_for('chatmain'))
+                        #Load user obj into session for use...
+                    else:
+                        flash('Bad UserName/Password', 'Failed')
+                        return redirect(url_for('login'))
+                except:
+                    flash('Bad UserName/Password', 'Failed')
+                    return redirect(url_for('login'))
+            except:
+                flash('Bad UserName/Password', 'Failed')
+                return redirect(url_for('login'))
 
         except:
             flash('Bad UserName/Password', 'Failed')
@@ -206,46 +206,46 @@ def SubmitNewUser():
         passwordCheck = str(req['passwordCheck'])
         dateCreated = str(datetime.now().isoformat())
 
-        # count = CryptoItems.encrypted_resource.scan(crypto_config=CryptoItems.custom_crypto_config)
-        # userID = int(len(count['Items']) + 1)
+        count = CryptoItems.encrypted_resource.scan(crypto_config=CryptoItems.custom_crypto_config)
+        userID = int(len(count['Items']) + 1)
 
-        # try:
-        #     response = CryptoItems.encrypted_resource.query(
-        #         KeyConditionExpression=Key('UserName').eq(userName),
-        #         crypto_config=CryptoItems.custom_crypto_config
-        #     )
+        try:
+            response = CryptoItems.encrypted_resource.query(
+                KeyConditionExpression=Key('UserName').eq(userName),
+                crypto_config=CryptoItems.custom_crypto_config
+            )
 
-        #     items = response['Items']
-        #     uniqueUser = items[0]['UserName']
+            items = response['Items']
+            uniqueUser = items[0]['UserName']
 
-        #     try:
-        #         if uniqueUser == userName:
-        #             flash('Bad User Already Exists!', 'Failed')
-        #             return redirect(url_for('register'))
-        #     except:
-        #         flash('Bad User Already Exists!', 'Failed')
-        #         return redirect(url_for('register'))
-        # except:
-        #     try:
-        #         if password == passwordCheck:
-        #             CryptoItems.encrypted_resource.put_item(
-        #                 TableName='Users',
-        #                 Item={'UserName': userName,
-        #                       'UserID': userID,
-        #                       'DateCreated': dateCreated,
-        #                       'UserEmail': userEmail,
-        #                       'password': password
-        #                       },
-        #                 crypto_config=CryptoItems.custom_crypto_config)
+            try:
+                if uniqueUser == userName:
+                    flash('Bad User Already Exists!', 'Failed')
+                    return redirect(url_for('register'))
+            except:
+                flash('Bad User Already Exists!', 'Failed')
+                return redirect(url_for('register'))
+        except:
+            try:
+                if password == passwordCheck:
+                    CryptoItems.encrypted_resource.put_item(
+                        TableName='Users',
+                        Item={'UserName': userName,
+                              'UserID': userID,
+                              'DateCreated': dateCreated,
+                              'UserEmail': userEmail,
+                              'password': password
+                              },
+                        crypto_config=CryptoItems.custom_crypto_config)
 
-        #             flash('Success! Please Login.', 'Success!')
-        #             return redirect(url_for('login'))
-        #         else:
-        #             flash('Passwords Do Not Match!', 'Failed')
-        #             return redirect(url_for('register'))
-        #     except:
-        #         flash('Passwords Do Not Match!', 'Failed')
-        #         return redirect(url_for('register'))
+                    flash('Success! Please Login.', 'Success!')
+                    return redirect(url_for('login'))
+                else:
+                    flash('Passwords Do Not Match!', 'Failed')
+                    return redirect(url_for('register'))
+            except:
+                flash('Passwords Do Not Match!', 'Failed')
+                return redirect(url_for('register'))
 
 #Added basic session info for use in template
 #May need to build out based on feedback
@@ -270,13 +270,13 @@ def broadcast_message(message):
     #TODO Somehow in here we need to see if sender is 
     #session user to determine response
     #message to make staggered chat window...
-        emit('response',
-        {
-            'data': message['data'],
-            'username': session['USERNAME'],
-            'initials': session['INITIALS']
-        },
-         broadcast=True)
+    emit('response',
+    {
+        'data': message['data'],
+        'username': session['USERNAME'],
+        'initials': session['INITIALS']
+    },
+        broadcast=True)
 
 #TODO Use for logoff?
 #FIXME This isn't working yet for some reason it immediately reconnects
